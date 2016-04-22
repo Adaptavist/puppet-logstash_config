67a867d53b3b77a818f9989fae6722fb889cb235
Matthew Hope <mhope@adaptavist.com>
2016-04-22 16:10:25 +0100

    added spec tests for patternfile

--- spec/classes/logstash_config_spec.rb
+++ spec/classes/logstash_config_spec.rb
@@ -25,4 +25,31 @@ describe 'logstash_config', :type => 'class' do
     end
     
   end
+
+  context "Should install logstash and adds pattern file to it" do
+    let(:params) { {
+        :package_url => package_url,
+        :config_file => '/tmp/logstash.conf',
+        :pattern_files => {  'pattern_file1' => { 'source' => 'puppet://files/pattern1' }, 'pattern_file2' => { 'source' => 'puppet://files/pattern2' } }
+        } }
+    let(:facts) {{
+      :kernel => 'Linux',
+      :operatingsystem => 'RedHat',
+      }}
+    it do
+      should contain_class('logstash').with(
+        'package_url'   => package_url,
+        'status'        => 'running',
+        'init_defaults' => { 
+            'START' => 'true'
+            },
+        'ensure'        => 'present',
+        'require'       => [],
+        )
+      should contain_logstash__configfile('logstash-config').with_source('/tmp/logstash.conf')
+      should contain_logstash__patternfile('pattern_file1').with_source('puppet://files/pattern1')
+      should contain_logstash__patternfile('pattern_file2').with_source('puppet://files/pattern2')
+    end
+    
+  end
 end
